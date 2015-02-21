2b2ee6ad8895f821462981e2cc1d3627f2578aee
sshtel <sshtel@gmail.com>
2015-02-21 23:48:38 +0900

    add Main.cpp update vc project files

---
+++ Win32/scene_change_detection/scene_change_detection/Main.cpp
@@ -0,0 +1,61 @@
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+
+void camera_test(){
+	
+    int id = -1;
+	for(int i=0; i<15; i++){
+		VideoCapture cap(i);
+		if(cap.isOpened()){
+			id = i; break;
+		}
+	}
+	CvCapture* capture = 0;
+    Mat frame, frameCopy, image;
+
+    capture = cvCaptureFromCAM( id ); //0=default, -1=any camera, 1..99=your camera
+    if(!capture) cout << "No camera detected" << endl;
+	
+
+    cvNamedWindow( "result", 1 );
+
+    if( capture )
+    {
+        cout << "In capture ..." << endl;
+        for(;;)
+        {
+            IplImage* iplImg = cvQueryFrame( capture );
+            frame = iplImg;
+            if( frame.empty() )
+                break;
+            if( iplImg->origin == IPL_ORIGIN_TL )
+                frame.copyTo( frameCopy );
+            else
+                flip( frame, frameCopy, 0 );
+
+
+			cv::imshow("result", frameCopy);
+            if( waitKey( 10 ) >= 0 )
+                cvReleaseCapture( &capture );
+        }
+	}
+        waitKey(0);
+
+    cvDestroyWindow("result");
+
+    return ;
+}
+
+int main( int argc, const char** argv )
+{
+	camera_test();
+	return 0;
+}
